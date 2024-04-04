683268b2580ada086ed33fd2099165315b91fb65
chinlinlee <a5566qq2581@gmail.com>
2024-04-04 17:31:53 +0800

    feat: add `generateUid` and `getFormattedDateTimeNow` in dicom utils

--- utils/dicom.js
+++ utils/dicom.js
@@ -26,4 +26,26 @@ async function storeInstance(buffer, stowUrl) {
     return true;
 }
 
+function generateUid() {
+    let uid = "2.25." + Math.floor(1 + Math.random() * 9);
+    for (let index = 0; index < 38; index++) {
+        uid = uid + Math.floor(Math.random() * 10);
+    }
+    return uid;
+}
+
+function getFormattedDateTimeNow() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+    return `${year}${month}${day}${hours}${minutes}${seconds}.${milliseconds}`;
+}
+
 module.exports.storeInstance = storeInstance;
+module.exports.generateUid = generateUid;
+module.exports.getFormattedDateTimeNow = getFormattedDateTimeNow;
